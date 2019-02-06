930a23da629a4c886a7d25ad0aa65e3714fdf3b1
翟洪毅 <329926648@qq.com>
2019-02-06 20:36:11 +0800

    0206

---
+++ src/leetcode/tree/dfs/DistributeCoins979.java
@@ -0,0 +1,19 @@
+package leetcode.tree.dfs;
+
+import leetcode.tree.TreeNode;
+
+/**
+ * Created by zhaiyi on 2019/2/1.
+ */
+public class DistributeCoins979 {
+    public int res = 0;
+    public int distributeCoins(TreeNode root) {
+
+    }
+
+    public int distribute(TreeNode node, int more) {
+        if (node != null) {
+            
+        }
+    }
+}
